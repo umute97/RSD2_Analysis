f419a346d08508131961f355cddcc946e8383d3a
LucaMenzio <luca.menzios@gmail.com>
2022-07-07 16:27:33 +0200

    small modification of test_rootfile arguments

--- test_newfiles/test_rootfile.py
+++ test_newfiles/test_rootfile.py
@@ -1,34 +1,41 @@
-from ROOT import TH2F, TFile, TTree, TCanvas, TH1F, TString
+import sys
 import ROOT
+from ROOT import TH2F, TFile, TTree, TCanvas, TH1F, TString
 from array import array
+from progress.bar import ChargingBar
 
 ROOT.EnableThreadSafety()
-ROOT.EnableImplicitMT()
+ROOT.EnableImplicitMT(8)
 
-nHists  = 5 
-step    = 20
-maxdimx = 1500
-maxdimy = 1500
+nHists  = 16 
+step    = 10
+maxdimx = 1100
+maxdimy = 700
 fHist     = []
 fCanvas   = []
-myFile = TFile.Open("/home/daq/hdd8TB/RSD2/stats_N_script/Run200_W3_croci1.3mm.root", "OPEN")
+
+if len(sys.argv) == 1:
+	filename = "../Run22_150V.root" #"../RunXX.root" # "/home/daq/hdd8TB/RSD2/stats_N_script/Run22_240.root" # " 
+else:
+	filename = sys.argv[1]
+myFile = TFile.Open(filename, "OPEN")
 tree = myFile.Get("Analysis")
 
 nEntries = tree.GetEntries()
-for j in range(0, nHists):
-	fHist.append(TH2F(str(j),"title",int(maxdimx/step),0,maxdimx,int(maxdimy/step),0,maxdimy))
+for j in range(nHists):
+	fHist.append(TH2F(str(j),"c"+str(j),int(maxdimx/step),0,maxdimx,int(maxdimy/step),0,maxdimy))
 print("created all histograms")
 
-count = 1
-for i in range(0, nEntries):
-	if( i == int(nEntries/10*count) ):
-		print("done with %d pc of the data"% (count*10))
-		count +=1
+bar = ChargingBar('Processing', max=nEntries, suffix = '%(percent)d%% [%(elapsed_td)s]')
+for i in range(nEntries):
 	tree.GetEntry(i)
-	for j in range(0, nHists): fHist[j].Fill(tree.XPos, tree.YPos, tree.ampl[j])
-	
-for j in range(0, nHists):
+	for j in range(nHists): fHist[j].Fill(tree.XPos, tree.YPos, tree.ampl[j])
+	bar.next()
+bar.finish()
+
+for j in range(nHists):
 	fCanvas.append(TCanvas(str("c%d"%j),str(j),600,600))
 	fHist[j].Draw("colz")
+	fCanvas[j].Modified()
 	fCanvas[j].Update()
 	fCanvas[j].SaveAs(str("c%d.pdf"%j))
